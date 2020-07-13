4d1fe5e569569900c2ad1f1f15b4c51611d52d48
tanakakoyo <tanaka@agile.co.jp>
2020-07-13 10:14:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-csxk63
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-csxk63)
---
+++ src/app/crisis-list/crisis-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crisis-list',
+  templateUrl: './crisis-list.component.html',
+  styleUrls: ['./crisis-list.component.css']
+})
+export class CrisisListComponent {
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
